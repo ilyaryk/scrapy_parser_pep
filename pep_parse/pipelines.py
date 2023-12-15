8109bde7a3ac80d2ef941d1da9b292c73b55fc43
ilyaryk <ilya.rykovanov@gmail.com>
2023-12-15 21:57:27 +0700

    1

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,8 +1,8 @@
-from itemadapter import ItemAdapter
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine, Column, Integer, Text
 from sqlalchemy.orm import Session
 import time
+
 
 class PepParsePipeline:
     def process_item(self, item, spider):
@@ -11,6 +11,7 @@ class PepParsePipeline:
 
 Base = declarative_base()
 
+
 class PEP(Base):
     __tablename__ = 'pep'
     id = Column(Integer, primary_key=True)
@@ -18,22 +19,23 @@ class PEP(Base):
     name = Column(Text)
     status = Column(Text)
 
+
 class PEPToDBPipeline:
     def open_spider(self, spider):
         engine = create_engine('sqlite:///sqlite.db')
         Base.metadata.create_all(engine)
         self.session = Session(engine)
-    
+
     def process_item(self, item, spider):
         pep = PEP(
-            number = item['number'],
+            number=item['number'],
             name=item['name'],
             status=item['status']
         )
         self.session.add(pep)
         self.session.commit()
         return item
-    
+
     def close_spider(self, spider):
         self.session.close()
 
@@ -44,25 +46,29 @@ class Status(Base):
     status = Column(Text)
     total = Column(Integer)
 
+
 class StatusToDBPipeline:
     statuses: dict = {}
+
     def open_spider(self, spider):
         engine = create_engine('sqlite:///sqlite.db')
         Base.metadata.create_all(engine)
         self.session = Session(engine)
-        
-    
+
     def process_item(self, item, spider):
-        self.statuses[item['status']] = self.statuses.get(item['status'], 0) + 1
+        self.statuses[item['status']] = self.statuses.get(
+            item['status'], 0) + 1
         return item
-    
+
     def close_spider(self, spider):
         now = time.localtime()
-        with open(f'results/status_summary_{time.strftime("%Y-%m-%d_%H_%M_%S", now)}.csv', mode='w', encoding='utf-8') as f:
+        now = time.strftime("%Y-%m-%d_%H_%M_%S", now)
+        with open(f'results/status_summary_{now}.csv',
+                  mode='w', encoding='utf-8') as f:
             f.write('Статус,Количество\n')
-            total=0
+            total = 0
             for i in self.statuses.keys():
-                total+=self.statuses[i]
+                total += self.statuses[i]
                 f.write(f'{i},{self.statuses[i]}\n')
             f.write(f'Total,{total}\n')
         self.session.close()
--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,6 +1,7 @@
 import scrapy
 from pep_parse.items import PepParseItem
 
+
 class PepSpider(scrapy.Spider):
     name = "pep"
     allowed_domains = ["peps.python.org"]
@@ -13,15 +14,15 @@ class PepSpider(scrapy.Spider):
         for pep_link in all_peps:
             yield response.follow(pep_link, callback=self.parse_pep)
 
-
     def parse_pep(self, response):
         resp = response.css('h1.page-title::text').get()
         num = int(resp.split()[1])
         title = resp.split()[3:]
-        status = response.css('dt:contains("Status") + dd').get().split('</abbr>')[0].split('>')[-1]
+        status = response.css(
+            'dt:contains("Status") + dd').get().split(
+                '</abbr>')[0].split('>')[-1]
         yield PepParseItem({
             'number': num,
             'name': ' '.join(title),
             'status': status
         })
-        
