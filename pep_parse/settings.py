be73e53affb67aed7490ded71f7eefc67ce2e4b1
ilyaryk <ilya.rykovanov@gmail.com>
2023-12-16 17:41:31 +0700

    1

--- pep_parse/settings.py
+++ pep_parse/settings.py
@@ -1,7 +1,4 @@
-from pathlib import Path
-
 BOT_NAME = "pep_parse"
-BASE_DIR = Path(__file__).parent
 SPIDER_MODULES = ["pep_parse.spiders"]
 NEWSPIDER_MODULE = "pep_parse.spiders"
 ROBOTSTXT_OBEY = True
@@ -9,7 +6,7 @@ REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
 FEEDS = {
-    f'{BASE_DIR}results/pep_%(time)s.csv': {
+    f'results/pep_%(time)s.csv': {
         'format': 'csv',
         'fields': ['number', 'name', 'status'],
         'overwrite': True
